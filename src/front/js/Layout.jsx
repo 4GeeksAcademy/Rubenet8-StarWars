ac7423be0af7021f12531268b6f4ccdc53c8556d
Rubeen.Frr8 <rubenferrermascarell@gmail.com>
2025-02-12 20:33:02 +0000

    Actualizacion

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -13,10 +13,7 @@ import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 
 
-//create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
@@ -29,7 +26,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} path= "contacts" />
+                        <Route element={<h1>Not found!</h1>} path= "*" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
